d87347fc808e5193a042df58ec9305e274b2a4d3
moneshongithub <monesh.lodhi123@gmail.com>
2023-02-19 23:10:07 +0530

    login page working

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(window).on('load',function(){
 	setTimeout(function(){ // allowing 3 secs to fade out loader
 	$('.page-loader').fadeOut('slow');
-	},3500);
+	},1500);
 });
 
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnpopup = document.querySelector('.btnLogin-popup');
+const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click',()=>{
@@ -18,25 +18,34 @@ loginLink.addEventListener('click',()=>{
 	wrapper.classList.remove('active')
 })
 
-btnpopup.addEventListener('click',()=>{
-	wrapper.classList.add('active-popup')
-})
 
 iconClose.addEventListener('click',()=>{
 	wrapper.classList.remove('active-popup')
 })
 
-var tl = gsap.timeline({
-	scrollTrigger: {
-		trigger: "#first",
-		start: "top top",
-		scrub: 1,
-		pin: true,
-		markers: true,
-	}
+btnPopup.addEventListener('click',()=>{
+	wrapper.classList.add('active-popup')
 })
 
-ti.
-to(
+
+
+iconClose.addEventListener('click',(dets)=>{
+
+	dets.style.display="none"
+})
+
+
+// var tl = gsap.timeline({
+// 	scrollTrigger: {
+// 		trigger: "#first",
+// 		start: "top top",
+// 		scrub: 1,
+// 		pin: true,
+// 		markers: true,
+// 	}
+// })
+
+// ti.
+// to(
 	
-)
+// )
