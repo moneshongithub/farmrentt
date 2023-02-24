e1cf76c71a04fffb26e56547b8ca7489b2a81382
moneshongithub <monesh.lodhi123@gmail.com>
2023-02-24 11:24:04 +0530

    fuckedup

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+
 // ####### loader ##########
+
+
+
 
 $(window).on('load',function(){
 	setTimeout(function(){ // allowing 3 secs to fade out loader
@@ -65,282 +69,434 @@ tl.to("#two", {
 },'one')
  
 
-var marketpage = document.querySelector("#marketpage")
+// var marketpage = document.querySelector("#marketpage")
 
-var market = document.querySelector("#centerdiv button")
-market.addEventListener("click",function(dets){
+// var market = document.querySelector("#centerdiv button")
+// market.addEventListener("click",function(dets){
 
-	document.querySelector("#marketloader").style.left="0%"
+// 	document.querySelector("#marketloader").style.left="0%"
 	
-	document.querySelector("#first").style.position="relative"       
-	document.querySelector("#marketpage").style.height="100vh"
-	document.querySelector("#second").style.height="0"
-	document.querySelector("#third").style.height="0"
-	document.querySelector("#fourth").style.height="0"
+// 	document.querySelector("#first").style.position="relative"       
+// 	document.querySelector("#marketpage").style.height="100vh"
+// 	document.querySelector("#second").style.height="0"
+// 	document.querySelector("#third").style.height="0"
+// 	document.querySelector("#fourth").style.height="0"
 
-	document.querySelector("#first").style.height="100vh"
-	
-
-
-
-
-	setTimeout(() => {
-		document.querySelector("#marketpage").style.transform="scale(1)"
-		document.querySelector("#marketpage").style.pointerEvents="all"
-
-		  }, "1500")
+// 	document.querySelector("#first").style.height="100vh"
 	
 
-	setTimeout(() => {
-	document.querySelector("#marketloader").style.left="100%"
-	  }, "1800")
 
-	  var tl2 = gsap.timeline();
-	  tl2.from("#mleft h1", {
-		x: -380,
-		ease: "power4.out",
-		delay: 2,
-		duration:2,
-		skewY: 10,
-		stagger: {
-		  amount: 0.3
-		}
-	  })
+
+
+// 	setTimeout(() => {
+// 		document.querySelector("#marketpage").style.transform="scale(1)"
+// 		document.querySelector("#marketpage").style.pointerEvents="all"
+
+// 		  }, "1500")
+	
+
+// 	setTimeout(() => {
+// 	document.querySelector("#marketloader").style.left="100%"
+// 	  }, "1800")
+
+// 	  var tl2 = gsap.timeline();
+// 	  tl2.from("#mleft h1", {
+// 		x: -380,
+		// ease: "power4.out",
+		// skewY: 10,
+// 		delay: 2,
+// 		duration:2,
+// 		stagger: {
+// 		  amount: 0.3
+// 		}
+// 	  })
 	  
-})
+// })
 
 
-var marketnav = document.querySelector("#bc")
-marketnav.addEventListener("click",function(dets){
+// var marketnav = document.querySelector("#bc")
+// marketnav.addEventListener("click",function(dets){
 
-	document.querySelector("#marketloader").style.left="0%"
+// 	document.querySelector("#marketloader").style.left="0%"
 	
-	document.querySelector("#first").style.position="relative"
-	document.querySelector("#marketpage").style.height="100vh"
-	document.querySelector("#second").style.height="0"
-	document.querySelector("#third").style.height="0"
-	document.querySelector("#fourth").style.height="0"
-	document.querySelector("#first").style.height="100vh"
+// 	document.querySelector("#first").style.position="relative"
+// 	document.querySelector("#marketpage").style.height="100vh"
+// 	document.querySelector("#second").style.height="0"
+// 	document.querySelector("#third").style.height="0"
+// 	document.querySelector("#fourth").style.height="0"
+// 	document.querySelector("#first").style.height="100vh"
 
 
 
-	setTimeout(() => {
-		document.querySelector("#marketpage").style.transform="scale(1)"
-		document.querySelector("#marketpage").style.pointerEvents="all"
+// 	setTimeout(() => {
+// 		document.querySelector("#marketpage").style.transform="scale(1)"
+// 		document.querySelector("#marketpage").style.pointerEvents="all"
 
-		  }, "1500")
+// 		  }, "1500")
 	
 
-	setTimeout(() => {
-	document.querySelector("#marketloader").style.left="100%"
-	  }, "1800")
+// 	setTimeout(() => {
+// 	document.querySelector("#marketloader").style.left="100%"
+// 	  }, "1800")
 
-	  gsap.from("#mleft h1", {
-		x: -380,
-		ease: "power4.out",
-		delay: 3,
-		duration:2,
-		skewY: 10,
-		stagger: {
-		  amount: 0.3
-		}
-	  })
+// 	  gsap.from("#mleft h1", {
+// 		x: -380,
+// 		ease: "power4.out",
+// 		delay: 3,
+// 		duration:2,
+// 		skewY: 10,
+// 		stagger: {
+// 		  amount: 0.3
+// 		}
+// 	  })
 	  
 	 
-})
+// })
 
 
 
-var marketicon = document.querySelector("#mlefticon").addEventListener("click",function(icon){
-	document.querySelector("#marketloader").style.left="0%"
-	document.querySelector("#first").style.position="initial"
-	document.querySelector("#second").style.height="100vh"
-	// document.querySelector("#mright").style.transform="scale(0)"
-	document.querySelector("#buy").style.transform="scale(0)"
-	document.querySelector(".rent").style.transform="scale(0)"
-	document.querySelector(".sell").style.transform="scale(0)"
-	// document.querySelector("#marketpage").style.height="140vh"
+// var marketicon = document.querySelector("#mlefticon").addEventListener("click",function(icon){
+// 	document.querySelector("#marketloader").style.left="0%"
+// 	document.querySelector("#first").style.position="initial"
+// 	document.querySelector("#second").style.height="100vh"
+// 	// document.querySelector("#mright").style.transform="scale(0)"
+// 	document.querySelector("#buy").style.transform="scale(0)"
+// 	document.querySelector(".rent").style.transform="scale(0)"
+// 	document.querySelector(".sell").style.transform="scale(0)"
+// 	// document.querySelector("#marketpage").style.height="140vh"
 
-	document.querySelector("#third").style.height="100vh"
-	document.querySelector("#first").style.height="140vh"
+// 	document.querySelector("#third").style.height="100vh"
+// 	document.querySelector("#first").style.height="140vh"
+// 	document.querySelector("#fourth").style.height="120vh"
 	
-	setTimeout(() => {
-		document.querySelector("#marketpage").style.transform="scale(0)"
-		document.querySelector("#marketpage").style.pointerEvents="none"
-		  }, "1500")
-	setTimeout(() => {
-		document.querySelector("#marketloader").style.left="-100%"
-		  }, "1800")
+// 	setTimeout(() => {
+// 		document.querySelector("#marketpage").style.transform="scale(0)"
+// 		document.querySelector("#marketpage").style.pointerEvents="none"
+// 		  }, "1500")
+// 	setTimeout(() => {
+// 		document.querySelector("#marketloader").style.left="-100%"
+// 		  }, "1800")
 		  
 		
-})
+// })
 
 
-
-
-
-
-
-var mlefticon = document.querySelector("#mleft .two").addEventListener("click",function(rent){
-	document.querySelector(".rent").style.transform="scale(1)"
-	document.querySelector("#buy").style.transform="scale(0)"
-	document.querySelector(".sell").style.transform="scale(0)"
+// // var mlefticon = document.querySelector("#mleft .two").addEventListener("click",function(rent){
+// // 	document.querySelector(".rent").style.transform="scale(1)"
+// // 	document.querySelector("#	").style.transform="scale(0)"
+// // 	document.querySelector(".sell").style.transform="scale(0)"
 
 	
-	var tl7 = gsap.timeline();
-	tl7.from(".rent", {
-		x: 1010,
-		ease: "power4.out",
-		delay: 0,		
-		duration:1,
-		opacity:0
+// // 	var tl7 = gsap.timeline();
+// // 	tl7.from(".rent", {
+// // 		x: 1010,
+// // 		ease: "power4.out",
+// // 		delay: 0,		
+// // 		duration:1,
+// // 		opacity:0
 	  
-	  })
-	tl7.from(".rent h2", {
-		y: 101,
-		ease: "power4.out",
-		delay: -0.5,		
-		duration:2,
-		opacity:0
+// // 	  })
+// // 	tl7.from(".rent h2", {
+// // 		y: 101,
+// // 		ease: "power4.out",
+// // 		delay: -0.5,		
+// // 		duration:2,
+// // 		opacity:0
 	  
-	  },'a')
+// // 	  },'a')
 	 
-	tl7.from(" #rentsteps h1 , p", {
-	  x: 850,
-	  ease: "power4.out",
-	  delay: -1.5,		
-	  duration:1,
-	  stagger:0.1
+// // 	tl7.from(" #rentsteps h1 , p", {
+// // 	  x: 850,
+// // 	  ease: "power4.out",
+// // 	  delay: -1.5,		
+// // 	  duration:1,
+// // 	  stagger:0.1
 	
-	},'a')
+// // 	},'a')
 
 
+// // })
+
+// var mlefticon1 = document.querySelector("#mleft .one").addEventListener("click",function(buy){
+// 	document.querySelector("#buy").style.transform="scale(1)"
+// 	document.querySelector(".rent").style.transform="scale(0)"
+// 	document.querySelector(".sell").style.transform="scale(0)"
+
+
+// 	var tl3 = gsap.timeline();
+// 	tl3.from("#buy h2", {
+// 		y: -101,
+// 		ease: "power4.out",
+// 		delay: 0,		
+// 		duration:1,
+// 		opacity:0
+	  
+// 	  })
+// 	tl3.from("#buy .buybox", {
+// 	  x: 1010,
+// 	  ease: "power4.out",
+// 	  delay: -0.6	,		
+// 	  duration:1,
+	
+// 	})
+	
+// })
+
+// var mlefticon2 = document.querySelector("#mleft .three").addEventListener("click",function(sell){
+// 	document.querySelector(".sell").style.transform="scale(1)"
+// 	document.querySelector("#buy").style.transform="scale(0)"
+// 	document.querySelector(".rent").style.transform="scale(0)"
+
+// 	var tl8 = gsap.timeline();
+// 	tl8.from(".sell ", {
+// 		x: 1010,
+// 		ease: "power4.out",
+// 		delay: 0,		
+// 		duration:1,
+	  
+// 	  })
+// 	tl8.from(".sell h2", {
+// 		y: 101,
+// 		ease: "power4.out",
+// 		delay: -0.5,		
+// 		duration:1,
+// 		opacity:0
+	  
+// 	  })
+	  
+
+// })
+
+
+
+//  var tl4 = gsap.timeline({
+// 	scrollTrigger:{
+// 		trigger:'#buy h2',
+// 		start:'top 78%',
+// 		end:'bottom bottom',
+// 		scrub:1,
+// 		markers:true,
+// 		y:"40",
+// 		duration:1,
+// 		opacity: 0,
+
+// 		}
+
+// })
+
+
+var tl2 = gsap.timeline({
+			scrollTrigger:{
+				start:'top 78%',
+				trigger:'#buy',
+				end:'bottom bottom',
+				scrub:1,
+				// markers:true
+		
+				}
+		})
+
+tl2 .from('#buy h2', {
+	duration:0.8,
+	opacity: 0,
+	y:"40",
+	ease: "power4.out",
+	skewY: 10,
+	
+})
+tl2 .from('.buybox', {
+	duration:0.8,
+	opacity: 0,
+	y:"40",
+	stagger:0.3
+	
+	
 })
 
-var mlefticon1 = document.querySelector("#mleft .one").addEventListener("click",function(buy){
-	document.querySelector("#buy").style.transform="scale(1)"
-	document.querySelector(".rent").style.transform="scale(0)"
-	document.querySelector(".sell").style.transform="scale(0)"
-
-
-	var tl3 = gsap.timeline();
-	tl3.from("#buy h2", {
-		y: -101,
-		ease: "power4.out",
-		delay: 0,		
-		duration:1,
-		opacity:0
-	  
-	  })
-	tl3.from("#buy .buybox", {
-	  x: 1010,
-	  ease: "power4.out",
-	  delay: -0.6	,		
-	  duration:1,
-	
-	})
-	
-})
-
-var mlefticon2 = document.querySelector("#mleft .three").addEventListener("click",function(sell){
-	document.querySelector(".sell").style.transform="scale(1)"
-	document.querySelector("#buy").style.transform="scale(0)"
-	document.querySelector(".rent").style.transform="scale(0)"
-
-	var tl8 = gsap.timeline();
-	tl8.from(".sell ", {
-		x: 1010,
-		ease: "power4.out",
-		delay: 0,		
-		duration:1,
-	  
-	  })
-	tl8.from(".sell h2", {
-		y: 101,
-		ease: "power4.out",
-		delay: -0.5,		
-		duration:1,
-		opacity:0
-	  
-	  })
-	  
-
-})
-
-
-
- var tl4 = gsap.timeline({
+var tl3 = gsap.timeline({
 	scrollTrigger:{
-		trigger:'#second',
-		 start:'top 78%',
-		  end:'bottom bottom',
-		   scrub:1,
-		//    markers:true
+		trigger:'.sell',
+		start:'top 78%',
+		end:'bottom bottom',
+		scrub:1,
+		// markers:true,
+		ease: "power4.out",
 
 		}
 })
-   tl4 .from('#second h1', {
+
+tl3 .from('.sell h2', {
+	duration:0.8,
+	opacity: 0,
+	y:"40",
+	stagger:0.2,
+	ease: "power4.out",
+	// skewY: 10,
+
+})
+tl3 .from('#sell1', {
+	duration:0.5,
+	opacity: 0,
+	y:"40",
+	delay:"-0.5"
+
+})
+tl3 .from('.sellleft', {
+	duration:0.5,
+	opacity: 0,
+	x:"-50",
+	delay:"-0.5"
+
+})
+tl3 .from('.sellright', {
+	duration:0.5,
+	opacity: 0,
+	x:"50",
+	delay:"-0.5"
+
+})
+
+var tl4 = gsap.timeline({
+	scrollTrigger:{
+		trigger:'.rent',
+		start:'top 78%',
+		end:'bottom bottom',
+		scrub:1,
+		// markers:true,
+		ease: "power4.out",
+
+		}
+})
+
+tl4 .from('.rent h2', {
+	duration:0.8,
+	opacity: 0,
+	y:"40",
+
+})
+tl4 .from('#rent1', {
+	duration:1,
+	opacity: 0,
+	y:"40",
+	backgroundColor:"white"
+
+})
+tl4 .from('#rentsteps h1', {
+	duration:0.8,
+	opacity: 0,
+	y:"40",
+
+})
+tl4 .from('#rentsteps p', {
+	duration:0.8,
+	opacity: 0,
+	x:"40",
+
+})
+tl4 .from('.rentleft', {
+	duration:0.8,
+	opacity: 0,
+	x:"-40",
+
+},'a')
+tl4 .from('.rentright', {
+	duration:0.8,
+	opacity: 0,
+	x:"40",
+
+},'a')
+
+
+
+
+var tl5 = gsap.timeline({
+	scrollTrigger:{
+		trigger:'#second',
+		start:'top 78%',
+		end:'bottom bottom',
+		scrub:1,
+		ease: "power4.out",
+		// markers:true
+
+		}
+})
+
+   tl5 .from('#second h1', {
 		y:"40",
-		duration:1,
+		duration:0.8,
 		opacity: 0,
 
 	})
 	.from('#second p', {
-		y:"40",
-		duration:1,
-		delay:"-0.3",
+		y:"30",
+		duration:0.8,
+		delay:"-0.5",
 		opacity: 0,
-	},)
+	})
 
-	var tl5 = gsap.timeline({
+	var tl6 = gsap.timeline({
 		scrollTrigger:{
 			trigger:'#third',
-			 start:'top 78%',
-			  end:'bottom bottom',
-			   scrub:1,
+			start:'top 78%',
+			end:'bottom bottom',
+			scrub:1,
+			ease: "power4.out",
+
 			//    markers:true
-	
 			}
 	})
-	   tl5 .from('#third h1', {
+	   tl6 .from('#third h1', {
 			y:"40",
-			duration:1,
+			duration:0.8,
 			opacity: 0,
-
-	
 		})
 		.from('#third p', {
 			y:"40",
-			duration:1,
+			duration:0.6,
 			delay:"-0.5",
 			opacity: 0,
 			
 		},)
 
-		.from('#third .slidebox', {
-			y:"40",
+		.to('#third .slidebox', {
+			y:"-50",
+			duration:1,
+			delay:"0",
+			// opacity: 0,
+			stagger:0.5
+			
+		},)
+
+.from('#third #slideboxleft', {
+			x:"-40",
 			duration:1,
 			delay:"0",
 			opacity: 0,
 			
 		},)
-
-		var tl6 = gsap.timeline({
+		.from('#third #slideboxright', {
+			x:"-40",
+			duration:1,
+			delay:"0",
+			opacity: 0,
+			
+		},)		
+		
+		var tl7 = gsap.timeline({
 			scrollTrigger:{
 				trigger:'#fourth',
-				 start:'top 85%',
-				  end:'bottom bottom',
-				   scrub:1,
+				start:'top 85%',
+				end:'bottom bottom',
+				scrub:1,
+				ease: "power4.out",
+
 				//    markers:true
-		
 				}
 		})
-		   tl6 .from('#fourth h1', {
-				y:"40",
+		   tl7 .from('#fourth h1', {
+				y:"-40",
 				duration:1,
 				opacity: 0,
-	
-		
 			})
 			.from('#fourth p', {
 				y:"40",
@@ -349,10 +505,43 @@ var mlefticon2 = document.querySelector("#mleft .three").addEventListener("click
 				opacity: 0,
 				
 			},)
+
+			.from('#fourth img', {
+				y:"40",
+				duration:1,
+				opacity: 0,
+				
+			},)
+			.from('#fourth .container input', {
+				x:"800",
+				ease:"power1",
+				duration:1,
+				opacity: 0,
+				stagger:0.2
+				
+			},)
+			.from('#fourth .container select', {
+				x:"800",
+				ease:"power1",
+
+				duration:1,
+				opacity: 0,
+				
+			},)
+			.from('#fourth .container textarea', {
+				x:"800",
+				ease:"power1",
+
+				duration:1,
+				opacity: 0,
+				
+			},)
 			.from('#fourth .Social-media ', {
-				y:"70",
+				x:"-1070",
 				duration:1,
 				delay:"0.5",
+				stagger:0.2,
+				ease:"power1",
 				// opacity: 0,
 				
 			},)
